e5116df4c1fcbd7cf7d0010a4f8de2d168a3e140
Yago Iglesias <yago.iglesias-vazquez@etu.u-paris.fr>
2022-12-02 15:03:24 +0100

    Naive implementation of residual calculations

--- ocaml/Residuels/bin/main.ml
+++ ocaml/Residuels/bin/main.ml
@@ -1,12 +1,14 @@
 type regex =
   | None
+  | Epsilon
   | Letter of char
   | Union of regex * regex
   | Concatenation of regex * regex
   | KleeneStar of regex
 
 let rec print_regex = function
-  | None -> print_string "None"
+  | None -> ()
+  | Epsilon -> ()
   | Letter c -> print_char c
   | Union (r1, r2) ->
       print_string "(";
@@ -24,9 +26,40 @@ let rec print_regex = function
       print_regex r;
       print_string ")*"
 
+let rec eps = function
+  | None -> false
+  | Epsilon -> true
+  | Letter _ -> false
+  | Union (r1, r2) -> eps r1 || eps r2
+  | Concatenation (r1, r2) -> eps r1 && eps r2
+  | KleeneStar _ -> true
+
+let rec residual_letter letter = function
+  | None -> None
+  | Epsilon -> None
+  | Letter c -> if c = letter then Epsilon else None
+  | Union (r1, r2) ->
+      let r1' = residual_letter letter r1 in
+      let r2' = residual_letter letter r2 in
+      Union (r1', r2')
+  | Concatenation (r1, r2) ->
+      let r1' = residual_letter letter r1 in
+      if eps r1 then
+        let r2' = residual_letter letter r2 in
+        Union (Concatenation (r1', r2), r2')
+      else Concatenation (r1', r2)
+  | KleeneStar r -> Concatenation (residual_letter letter r, KleeneStar r)
+
 let () =
   let r =
-    Concatenation (Letter 'a', KleeneStar (Union (Letter 'b', Letter 'c')))
+    Concatenation
+      ( Concatenation
+          ( Concatenation
+              (Letter 'a', KleeneStar (Union (Letter 'b', Letter 'c'))),
+            Epsilon ),
+        None )
   in
   print_regex r;
-  print_newline ()
+  print_newline ();
+  residual_letter 'a' r |> print_regex;
+  residual_letter 'a' r |> residual_letter 'a' |> print_regex
--- ocaml/residuels/bin/main.ml
+++
@@ -1,2 +0,0 @@
-type ExpRat ={ None; char c ; ExpRat * ExpRat ; ExpRat | ExpRat ; Epsilon}
-;;
