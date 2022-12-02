8a9156d4b6a35616a91ac2f30944820972bf8a07
Yago Iglesias <yago.iglesias-vazquez@etu.u-paris.fr>
2022-12-03 00:23:53 +0100

    Add string residual

--- ocaml/Residuels/bin/main.ml
+++ ocaml/Residuels/bin/main.ml
@@ -34,6 +34,38 @@ let rec eps = function
   | Concatenation (r1, r2) -> eps r1 && eps r2
   | KleeneStar _ -> true
 
+let rec is_null = function
+  | None -> true
+  | Epsilon -> false
+  | Letter _ -> false
+  | Union (r1, r2) -> is_null r1 && is_null r2
+  | Concatenation (r1, r2) -> is_null r1 || is_null r2
+  | KleeneStar r -> is_null r
+
+let rec simplify = function
+  | None -> None
+  | Epsilon -> Epsilon
+  | Letter c -> Letter c
+  | Union (r1, r2) ->
+      let r1 = simplify r1 in
+      let r2 = simplify r2 in
+      if eps r1 then if eps r2 then Epsilon else r2
+      else if eps r2 then r1
+      else Union (r1, r2)
+  | Concatenation (r1, r2) ->
+      let r1 = simplify r1 in
+      let r2 = simplify r2 in
+      let r1_null = is_null r1 in
+      let r2_null = is_null r2 in
+      if r1_null then if r2_null then None else r2
+      else if r2_null then r1
+      else if eps r1 then if eps r2 then Epsilon else r2
+      else if eps r2 then r1
+      else Concatenation (r1, r2)
+  | KleeneStar r ->
+      let r = simplify r in
+      KleeneStar r
+
 let rec residual_letter letter = function
   | None -> None
   | Epsilon -> None
@@ -50,6 +82,17 @@ let rec residual_letter letter = function
       else Concatenation (r1', r2)
   | KleeneStar r -> Concatenation (residual_letter letter r, KleeneStar r)
 
+let rec residual_string s regex =
+  match s with
+  | "" -> Epsilon
+  | s ->
+      if String.length s = 1 then residual_letter s.[0] regex
+      else
+        let c = (String.sub s 0 1 |> String.lowercase_ascii |> String.get) 0 in
+        Concatenation
+          ( residual_letter c regex,
+            residual_string (String.sub s 1 (String.length s - 1)) regex )
+
 let () =
   let r =
     Concatenation
@@ -59,7 +102,14 @@ let () =
             Epsilon ),
         None )
   in
+  print_newline ();
   print_regex r;
   print_newline ();
   residual_letter 'a' r |> print_regex;
-  residual_letter 'a' r |> residual_letter 'a' |> print_regex
+  print_newline ();
+  residual_letter 'a' r |> residual_letter 'a' |> print_regex;
+  print_newline ();
+  residual_letter 'a' r |> residual_letter 'a' |> simplify |> print_regex;
+  print_newline ();
+  residual_string "a" r |> print_regex;
+  print_newline ()
